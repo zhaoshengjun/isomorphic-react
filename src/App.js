522ef3fbd6de56e68706598391ba2764a883815f
Sam Zhao <zhao.shengjun@gmail.com>
2017-02-23 16:21:14 +1000

    add links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 export const App = () => (
   <div>
-    Hello React Router!
+    <h1>Hello React Router!</h1>
+    <ul role="nav">
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/repos">Repos</Link>
+      </li>
+    </ul>
   </div>
 )
