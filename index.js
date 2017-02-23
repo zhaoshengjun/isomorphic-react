35466ac796bf3f1b63a2bae620d2e42648ff4306
Sam <zhao.shengjun@gmail.com>
2017-02-23 23:07:22 +1000

    history api fallback

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
-import { Router, Route, hashHistory, IndexRoute } from "react-router";
+import { Router, Route, browserHistory, IndexRoute } from "react-router";
 import { App } from "./src/App";
 import { About } from "./src/About";
 import { Repos } from "./src/Repos";
 import { Repo } from "./src/Repo";
-import {Home} from './src/Home';
+import { Home } from "./src/Home";
 
 render(
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={Home}></IndexRoute>
+      <IndexRoute component={Home} />
       <Route path="/about" component={About} />
       <Route path="/repos" component={Repos}>
         <Route path="/repos/:userName/:repoName" component={Repo} />
