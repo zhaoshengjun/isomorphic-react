d3a9dba6c3ee8a45a4bd3d52616e71b2497920ce
Sam <zhao.shengjun@gmail.com>
2017-02-23 22:40:37 +1000

    use acitveClass and activeStyle

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router';
+import {NavLink} from './NavLink';
 
 export const App = (props) => ( 
   <div>
     <h1>Hello React Router!</h1>
     <ul role="nav">
       <li>
-        <Link to="/about">About</Link>
+        <Link to="/about" activeClassName="active">About</Link>
       </li>
       <li>
-        <Link to="/repos">Repos</Link>
+        <Link to="/repos" activeStyle={{color:'red'}}>Repos</Link>
       </li>
     </ul>
     {props.children} 
---
+++ src/NavLink.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+export const NavLink = (props) => (
+  <Link {...props} activeClassName="active">
+  </Link>
+)
