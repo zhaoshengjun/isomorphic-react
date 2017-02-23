5bdc86d9b495e6c7fb39afc928c3a97615636285
Sam <zhao.shengjun@gmail.com>
2017-02-23 23:01:46 +1000

    IndexLink

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {IndexLink} from 'react-router';
 import {NavLink} from './NavLink';
 import {Home} from './Home';
 
@@ -6,6 +7,9 @@ export const App = (props) => (
   <div>
     <h1>Hello React Router!</h1>
     <ul role="nav">
+      <li>
+        <NavLink to="/" onlyActiveOnIndex={true} >Home</NavLink>
+      </li>
       <li>
         <NavLink to="/about">About</NavLink>
       </li>
